94e2f8a3d6c4747aa8343e25869b848da5fcddad
rrathang <r.rathanganga@gmail.com>
2025-11-15 00:43:20 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-let villasData = [];
+let villas = [];
 
-// Load the JSON file
+// Load JSON
 fetch("villas.json")
-  .then(response => response.json())
+  .then(res => res.json())
   .then(data => {
-      villasData = data.villas;
+      villas = data;
       loadVillaCards();
   });
 
-// Create villa cards dynamically
+// Render cards
 function loadVillaCards() {
     const container = document.querySelector(".container");
     container.innerHTML = "";
 
-    villasData.forEach(villa => {
+    villas.forEach(villa => {
         const card = document.createElement("div");
         card.classList.add("card");
         card.onclick = () => openModal(villa);
 
         card.innerHTML = `
-            <img src="${villa.images[0]}" alt="">
-            <h3>${villa.title}</h3>
+            <img src="${villa.images[0]}">
+            <h3>${villa.name}</h3>
             <p>${villa.location}</p>
             <button>View Details</button>
         `;
@@ -29,16 +29,39 @@ function loadVillaCards() {
     });
 }
 
-// Open modal with villa info
+// Open modal
 function openModal(villa) {
-    document.getElementById("villaTitle").innerText = villa.title;
+    document.getElementById("villaName").innerText = villa.name;
     document.getElementById("villaLocation").innerText = villa.location;
-    document.getElementById("villaPrice").innerText = villa.price;
     document.getElementById("villaDescription").innerText = villa.description;
 
+    // Types
+    const typeContainer = document.getElementById("villaTypes");
+    typeContainer.innerHTML = "";
+    villa.type.forEach(t => {
+        const tag = document.createElement("span");
+        tag.classList.add("tag");
+        tag.innerText = t;
+        typeContainer.appendChild(tag);
+    });
+
+    // Rooms, pool, beachside
+    document.getElementById("villaRooms").innerText = `Rooms: ${villa.rooms}`;
+    document.getElementById("villaPool").innerText = `Private Pool: ${villa.pool ? "Yes" : "No"}`;
+    document.getElementById("villaBeachside").innerText = `Beachside: ${villa.beachside ? "Yes" : "No"}`;
+
+    // Amenities
+    const list = document.getElementById("villaAmenities");
+    list.innerHTML = "";
+    villa.amenities.forEach(a => {
+        const li = document.createElement("li");
+        li.innerText = a;
+        list.appendChild(li);
+    });
+
+    // Gallery
     const gallery = document.getElementById("villaGallery");
     gallery.innerHTML = "";
-
     villa.images.forEach(img => {
         const image = document.createElement("img");
         image.src = img;
